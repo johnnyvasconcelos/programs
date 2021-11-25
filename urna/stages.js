53330af590767e43731c1c5a682b3c7c754bfcb2
Johnny Vasconcelos <91664887+johnnyvasconcelos@users.noreply.github.com>
2021-11-25 12:28:09 -0200

    atualização 1
    adicionando candidatos e o início do script;

---
+++ urna/stages.js
@@ -0,0 +1,50 @@
+let stages = [
+    {
+titulo: 'Vereador',
+numeros: 5,
+candidatos: [
+    {
+        numero: 68000,
+        nome: 'Lívio Silva',
+        partido: 'PLS',
+        fotos: [
+            {url: 'vereador1.png'}
+        ]
+    },
+    {
+        numero: 59000,
+        nome: 'Maurícia Campos',
+        partido: 'PMC',
+        fotos: [
+            {url: 'vereador2.png', legenda: 'Vereadora'}
+        ]
+    },
+]
+},
+{
+titulo: 'Prefeito',
+numeros: 2,
+candidatos: [
+    {
+        numero: 68,
+        nome: 'Luma Santos',
+        partido: 'PLS',
+        vice: 'Lamar',
+        fotos: [
+            {url: 'prefeito1.png', legenda: 'Prefeita'},
+            {url: 'vice1.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+    },
+    {
+        numero: 59,
+        nome: 'Miranda Costa',
+        partido: 'PMC',
+        vice: 'Mirandir',
+        fotos: [
+            {url: 'prefeito2.png', legenda: 'Prefeito'},
+            {url: 'vice2.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+    },
+]
+}
+];
